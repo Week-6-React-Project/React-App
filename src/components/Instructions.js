c23ad0f3edb6e6f96872b0d24d00465cd374a2bb
MustafovaSemra <mustafovasemra@yahoo.com>
2021-07-06 15:04:37 -0400

    added recipes

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,48 +1,44 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Instructions.css";
 
+function Instructions({ id }) {
+  const [recipeDetails, setRecipeDetails] = useState({});
 
-function Instructions({id}) {
-    
- const [recipeDetails, setRecipeDetails] = useState({})
+  const searchByIngredient = async () => {
+    console.log("click");
+    let res = await axios.get(
+      `https://api.spoonacular.com/recipes/${id}/information?apiKey=c40a678376e24366886a26ba00de5aab`
+    );
 
-const searchByIngredient = async () => {
+    setRecipeDetails(res.data);
+  };
 
-console.log("click") 
-let res = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=c40a678376e24366886a26ba00de5aab`)
-      
-setRecipeDetails(res.data)
-      
-}
+  useEffect(() => {
+    searchByIngredient();
+  }, []);
 
-useEffect(()=>{
-searchByIngredient()
-
-},[])
-
-
-const showRecipeDetails = () => {
-    return(
-      <div>
-          {recipeDetails.diets?.map(each => {
-        return (
-            <li>{each}</li>
-        )
+  const showRecipeDetails = () => {
+    return (
+      <div className="ingredient">
+        <ul>
+          {recipeDetails.diets?.map((each) => {
+            return <li>{each}</li>;
           })}
+        </ul>
+        <div className="ready-servings">
           <p>Ready in: {recipeDetails.readyInMinutes} min</p>
           <p>Servings: {recipeDetails.servings} </p>
-          <p dangerouslySetInnerHTML={{__html:recipeDetails.summary,}}></p>
-      </div>
-
-    )
-}
-
-    return (
-        <div>
-            {showRecipeDetails()}
-            
         </div>
-    )
+        <p
+          className="instructions"
+          dangerouslySetInnerHTML={{ __html: recipeDetails.summary }}
+        ></p>
+      </div>
+    );
+  };
+
+  return <div>{showRecipeDetails()}</div>;
 }
 
-export default Instructions
+export default Instructions;
