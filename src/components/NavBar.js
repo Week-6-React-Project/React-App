0e37be9c86e0235583e841982713c8ebf951fcbf
Vojislav Zaja <81269127+vojaso82@users.noreply.github.com>
2021-07-08 15:34:44 -0400

    hamburger

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+
+
 
 function NavBar({ scrollToAbout, scrollToTeam, scrollToHome }) {
+  
+  const [toggle, setToggle] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  console.log(toggle)
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth) 
+    handleToggle()
+  }
+
+ const handleToggle = () => {
+   if(window.innerWidth < 1000){
+     setToggle(false)
+   }
+   if(window.innerWidth >= 1000){
+     setToggle(true)
+   }
+ }
+
+useEffect (() => {
+  handleResize()
+  window.addEventListener("resize", handleResize)
+}, [window.innerWidth])
+ 
+
   return (
     <nav>
+      
       <div className="header-logo">
         <Link to="/">
         <img src="../../images/navbar-new2.png" />
         </Link>
        
-
+    
       </div>
-      <ul>
+      <div className = "burger" onClick={()=>setToggle(!toggle)}>
+        {toggle &&  <FontAwesomeIcon icon={faTimes}/> || <FontAwesomeIcon icon={faBars}/>}
+     
+     
+      </div>
+      {toggle &&  <ul className="big-screen">
         <li>
           <Link onClick={scrollToHome} to="/">Home</Link>
         </li>
@@ -34,7 +70,8 @@ function NavBar({ scrollToAbout, scrollToTeam, scrollToHome }) {
         <li onClick={scrollToTeam}>
           <Link to="/">Team</Link>
         </li>
-      </ul>
+      </ul>}
+     
     </nav>
   );
 }
